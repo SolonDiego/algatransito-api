ca92ad7a73961bd73c2ac2bb1ce9fc8089f18e50
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2025-02-02 20:35:07 -0300

    update

---
+++ src/main/resources/db/migration/V004__inserir-proprietarios.sql
@@ -0,0 +1,11 @@
+INSERT INTO proprietario VALUES
+(NULL,'João da Silva Sousa','joaosilva@gmail.com','11 98888-0000'),
+(NULL,'Maria Abadia','mariaabadia@gmail.com','34 98222-1111'),
+(NULL,'Fernando','fernando@gmail.com','11 91122-3333'),
+(NULL,'Sebastião','sebastiao@gmail.com','34 99333-3333'),
+(NULL,'Solon Diego Garcia Moreira','solondiego@gmail.com','83 99128-9751'),
+(NULL,'Maria Alicia','mariaalicia@gmail.com','83 98888-1111'),
+(NULL,'Zé Para Exluir','zeparaexluir@gmail.com','83 98888-1111'),
+(NULL,'João Lucas Gomes Moreira','joaolucas@gmail.com','83 99312-5583'),
+(NULL,'Miqueline Gomes Moreira','miqueline@gmail.com','83 98888-1111'),
+(NULL,'Fulano de Tal','fulanodetal@hotmail.com','83 99999-2222');
---
+++ src/main/resources/db/migration/V006__inserir-autuacoes.sql
@@ -0,0 +1,4 @@
+INSERT INTO autuacao VALUES
+(NULL,1,'Avançou o sinal vermelho', 450.00,'2025-01-21 18:49:39'),
+(NULL,1,'Avançou faixa de pedestres', 200.00,'2025-02-02 20:10:14'),
+(NULL,3,'Trafegava a 20% a mais da velocidade máxima',380.00,'2025-02-01 17:18:54');
